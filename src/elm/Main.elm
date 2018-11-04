00c3b0c55f3762b037e5a9f767971684745e4919
theochupp <tclchiam@gmail.com>
2018-11-04 14:41:25 -0500

    Separated Router into a different module than Main

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -8,15 +8,14 @@ import Page.Closet
 import Page.Home
 import Page.Login
 import Page.NotFound
+import Router
 import TopBar
 import Url exposing (Url)
 import Url.Parser as UrlParser exposing ((</>), Parser, fragment, s, string, top)
 
 
 type alias Model =
-    { navKey : Navigation.Key
-    , route : Route
-    }
+    { routerModel : Router.Model }
 
 
 
@@ -26,13 +25,21 @@ type alias Model =
 init : () -> Url -> Navigation.Key -> ( Model, Cmd Msg )
 init _ url key =
     let
-        ( model, urlCmd ) =
-            urlUpdate url { navKey = key, route = Home }
+        ( routerModel, routerCmd ) =
+            Router.init () url key
 
         ( topBarModel, topBarCmd ) =
             TopBar.init
+
+        model =
+            { routerModel = routerModel }
     in
-    ( model, Cmd.batch [ urlCmd, topBarCmd ] )
+    ( model
+    , Cmd.batch
+        [ Cmd.map RouterMsg routerCmd
+        , topBarCmd
+        ]
+    )
 
 
 
@@ -49,28 +56,18 @@ subscriptions model =
 
 
 type Msg
-    = UrlChanged Url
-    | LinkClicked UrlRequest
+    = RouterMsg Router.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        LinkClicked req ->
-            case req of
-                Browser.Internal url ->
-                    ( model, Navigation.pushUrl model.navKey <| Url.toString url )
-
-                Browser.External href ->
-                    ( model, Navigation.load href )
-
-        UrlChanged url ->
-            urlUpdate url model
-
-
-urlUpdate : Url -> Model -> ( Model, Cmd Msg )
-urlUpdate url model =
-    ( { model | route = decode url }, Cmd.none )
+        RouterMsg submsg ->
+            let
+                ( routerModel, routerCmd ) =
+                    Router.update submsg model.routerModel
+            in
+            ( { model | routerModel = routerModel }, Cmd.map RouterMsg routerCmd )
 
 
 
@@ -87,53 +84,34 @@ view model =
 
 mainContent : Model -> Html Msg
 mainContent model =
-    case model.route of
-        Home ->
+    case model.routerModel.route of
+        Router.Home ->
             Page.Home.view ()
 
-        Login ->
+        Router.Login ->
             Page.Login.view ()
 
-        Closet _ ->
+        Router.Closet _ ->
             Page.Closet.view ()
 
-        NotFound ->
+        Router.NotFound ->
             Page.NotFound.pageNotFound ()
 
 
 
--- ROUTES
-
-
-type Route
-    = Home
-    | Closet String
-    | Login
-    | NotFound
-
-
-decode : Url -> Route
-decode url =
-    let
-        path =
-            Maybe.withDefault "" url.fragment
-    in
-    { url | path = url.path, fragment = Nothing }
-        |> UrlParser.parse routeParser
-        |> Maybe.withDefault NotFound
-
-
-routeParser : Parser (Route -> a) a
-routeParser =
-    UrlParser.oneOf
-        [ UrlParser.map Home top
-        , UrlParser.map Login (s "login" </> top)
-        , UrlParser.map Closet (s "closet" </> string)
-        ]
-
-
-
 -- MAIN
+
+
+onUrlRequest : UrlRequest -> Msg
+onUrlRequest urlRequest =
+    Router.LinkClicked urlRequest
+        |> RouterMsg
+
+
+onUrlChange : Url -> Msg
+onUrlChange url =
+    Router.UrlChanged url
+        |> RouterMsg
 
 
 main : Program () Model Msg
@@ -143,6 +121,6 @@ main =
         , view = view
         , update = update
         , subscriptions = subscriptions
-        , onUrlRequest = LinkClicked
-        , onUrlChange = UrlChanged
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
         }
