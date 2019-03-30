b4bff3f1892d957d0bdfed426679835858d71e7e
theochupp <tclchiam@gmail.com>
2019-03-29 22:37:57 -0400

    Added new webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
-module.exports = {
-    entry: './src/static/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                loader: 'file-loader?name=[name].[ext]'
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const modeConfig = env => require(`./build-tools/webpack.${env}`)(env);
+const presetConfig = require("./build-tools/loadPresets");
+
+module.exports = ({mode, presets} = {mode: "production", presets: []}) => {
+    console.log(`Building for: ${mode}`);
+
+    return webpackMerge({
+            mode: mode,
+            entry: path.join(__dirname, './src/static/index.js'),
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/,
+                        exclude: /node_modules/,
+                        loader: 'file-loader?name=[name].[ext]'
+                    },
+                    {
+                        test: /\.(eot|svg|ttf|woff|woff2)$/,
+                        loader: 'file-loader'
+                    }
+                ]
             },
-            {
-                test: /\.elm$/,
-                exclude: [/elm-stuff/, /node_modules/],
-                loader: 'elm-webpack-loader',
-                options: {
-                    debug: true
-                }
-            },
-            {
-                test: /\.less$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'less-loader'
-                }]
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    devServer: {
-        inline: true,
-        contentBase: './src/static',
-        stats: 'errors-only'
-    }
+            plugins: [
+                // new HtmlWebpackPlugin({
+                //     template: 'src/static/index.html',
+                //     inject: 'body',
+                //     filename: 'index.html',
+                // }),
+
+                new StyleLintPlugin(),
+
+                // new CopyWebpackPlugin([
+                //     { from: 'src/assets/favicon.ico' }
+                // ]),
+            ]
+        },
+        modeConfig(mode),
+        presetConfig({mode: mode, presets: presets})
+    );
 };
